d446c5c285e75786f81a41b60f48073bd23df71a
Abhimanyu <karcheabhimanyu@gmail.com>
2025-08-15 00:52:33 +0530

    backend issue solved

--- backend/tiffin_trail_backend/src/main/java/com/cdac/Security/AppConfig.java
+++ backend/tiffin_trail_backend/src/main/java/com/cdac/Security/AppConfig.java
@@ -1,14 +1,14 @@
 package com.cdac.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
-
-  
-//    public PasswordEncoder passwordEncoder() {
+//@Bean
+//   public PasswordEncoderr passwordEncoderr() {
 //        return new BCryptPasswordEncoder();
 //    }
 }
